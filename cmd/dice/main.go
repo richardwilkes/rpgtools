9d2d7f12d0b219c3876a0de84af7ba8e4be62173
Richard Wilkes <wilkes@me.com>
2020-01-10 14:16:10 -0800

    Add basic command line

--- cmd/dice/main.go
+++ cmd/dice/main.go
@@ -12,12 +12,27 @@ package main
 import (
 	"fmt"
 	"os"
+	"time"
 
 	"github.com/dustin/go-humanize"
 	"github.com/richardwilkes/rpgtools/dice"
+	"github.com/richardwilkes/toolbox/cmdline"
+	"github.com/richardwilkes/toolbox/i18n"
 )
 
 func main() {
+	if cmdline.License == "" {
+		cmdline.License = "Mozilla Public License, version 2.0"
+	}
+	if cmdline.CopyrightYears == "" {
+		cmdline.CopyrightYears = fmt.Sprintf("2017-%d", time.Now().Year())
+	}
+	if cmdline.CopyrightHolder == "" {
+		cmdline.CopyrightHolder = "Richard A. Wilkes"
+	}
+	cl := cmdline.New(true)
+	cl.UsageSuffix = i18n.Text("[dice expression]...")
+	cl.Parse(os.Args[1:])
 	for _, arg := range os.Args[1:] {
 		d := dice.New(nil, arg)
 		fmt.Printf("%v = %s\n", d, humanize.Comma(int64(d.Roll())))
