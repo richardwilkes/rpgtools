3feb6049fbfc02f1a298d3f17338171e297da13f
Richard Wilkes <wilkes@me.com>
2023-02-15 17:37:22 -0800

    Update to golangci-lint v1.51.1

--- build.sh
+++ build.sh
@@ -3,7 +3,7 @@ set -eo pipefail
 
 trap 'echo -e "\033[33;5mBuild failed on build.sh:$LINENO\033[0m"' ERR
 
-GOLANGCI_LINT_VERSION=1.48.0
+GOLANGCI_LINT_VERSION=1.51.1
 RACE=-race
 
 for arg in "$@"
