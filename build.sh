cf8d19473255333e6927fbb6c4d4e9909ee0d296
Richard Wilkes <wilkes@me.com>
2023-04-01 15:03:20 -0700

    Remove dice command generation

--- build.sh
+++ build.sh
@@ -3,9 +3,6 @@ set -eo pipefail
 
 trap 'echo -e "\033[33;5mBuild failed on build.sh:$LINENO\033[0m"' ERR
 
-GOLANGCI_LINT_VERSION=1.51.1
-RACE=-race
-
 for arg in "$@"
 do
   case "$arg" in
@@ -29,38 +26,22 @@ do
   esac
 done
 
-# Setup version info
-if command -v git 2>&1 > /dev/null; then
-  if [ -z "$(git status --porcelain)" ]; then
-    STATE=clean
-  else
-    STATE=dirty
-  fi
-  GIT_VERSION=$(git rev-parse HEAD)-$STATE
-  GIT_TAG=$(git tag --points-at HEAD)
-  if [ -z "$GIT_TAG" ]; then
-    GIT_TAG=$(git tag --list --sort -version:refname | head -1)
-    if [ -n "$GIT_TAG" ]; then
-      GIT_TAG=$GIT_TAG~
-    fi
-  fi
-  if [ -n "$GIT_TAG" ]; then
-    VERSION=$(echo "$GIT_TAG" | sed -E "s/^v//")
-  else
-    VERSION=""
-  fi
-else
-  GIT_VERSION=Unknown
-  VERSION=""
-fi
-
 # Build the code
-echo -e "\033[33mBuilding Go code...\033[0m"
-LINK_FLAGS="-X github.com/richardwilkes/toolbox/cmdline.AppVersion=$VERSION"
-LINK_FLAGS="$LINK_FLAGS -X github.com/richardwilkes/toolbox/cmdline.BuildNumber=$(date -u "+%Y%m%d%H%M%S")"
-LINK_FLAGS="$LINK_FLAGS -X github.com/richardwilkes/toolbox/cmdline.GitVersion=$GIT_VERSION"
-LINK_FLAGS="$LINK_FLAGS -X github.com/richardwilkes/toolbox/cmdline.CopyrightYears=2017-$(date "+%Y")"
-go build -v -ldflags=all="$LINK_FLAGS" ./...
+echo -e "\033[33mBuilding...\033[0m"
+go build -v ./...
+
+# Run the linters
+if [ "$LINT"x == "1x" ]; then
+  GOLANGCI_LINT_VERSION=1.51.1
+  TOOLS_DIR=$PWD/tools
+  if [ ! -e "$TOOLS_DIR/golangci-lint" ] || [ "$("$TOOLS_DIR/golangci-lint" version 2>&1 | awk '{ print $4 }' || true)x" != "${GOLANGCI_LINT_VERSION}x" ]; then
+    echo -e "\033[33mInstalling version $GOLANGCI_LINT_VERSION of golangci-lint into $TOOLS_DIR...\033[0m"
+    mkdir -p "$TOOLS_DIR"
+    curl -sfL https://raw.githubusercontent.com/golangci/golangci-lint/master/install.sh | sh -s -- -b "$TOOLS_DIR" v$GOLANGCI_LINT_VERSION
+  fi
+  echo -e "\033[33mLinting...\033[0m"
+  "$TOOLS_DIR/golangci-lint" run
+fi
 
 # Run the tests
 if [ "$TEST"x == "1x" ]; then
@@ -71,19 +52,3 @@ if [ "$TEST"x == "1x" ]; then
   fi
   go test $RACE ./...
 fi
-
-# Run the linters
-if [ "$LINT"x == "1x" ]; then
-  TOOLS_DIR=$PWD/tools
-  if [ ! -e "$TOOLS_DIR/golangci-lint" ] || [ "$("$TOOLS_DIR/golangci-lint" version 2>&1 | awk '{ print $4 }' || true)x" != "${GOLANGCI_LINT_VERSION}x" ]; then
-    echo -e "\033[33mInstalling version $GOLANGCI_LINT_VERSION of golangci-lint into $TOOLS_DIR...\033[0m"
-    mkdir -p "$TOOLS_DIR"
-    curl -sfL https://raw.githubusercontent.com/golangci/golangci-lint/master/install.sh | sh -s -- -b "$TOOLS_DIR" v$GOLANGCI_LINT_VERSION
-  fi
-  echo -e "\033[33mRunning Go linters...\033[0m"
-  "$TOOLS_DIR/golangci-lint" run --config .golangci.yml 2> >(grep -Ev "The linter '(interfacer|maligned)' is deprecated")
-fi
-
-# Install executables
-echo -e "\033[33mInstalling executables...\033[0m"
-go install -ldflags=all="$LINK_FLAGS" ./cmd/dice
